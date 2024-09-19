77ad01280463724bf3ff496dc2d1f9dbbcb37f92
Your Name <mbremer2@st.noorderpoort.nl>
2024-09-19 12:05:28 +0200

    fixed toast error

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import Toast from 'primevue/toast';
 	<div class="flex justify-content-center align-items-center min-h-screen">
 		<div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
 			<h1 class="text-center mb-4">Record Finder</h1>
-			<Toast />
-			<RecordFinder />
+			<Toast></Toast>
+			<RecordFinder></RecordFinder>
 		</div>
 	</div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import App from './App.vue';
 
+
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import ProgressSpinner from 'primevue/progressspinner'
+import Card from 'primevue/card'
+import Dialog from 'primevue/dialog'
+import MultiSelect from 'primevue/multiselect'
+import Checkbox from 'primevue/checkbox'
+import Toast from 'primevue/toast'
 import './style.css'
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
@@ -12,7 +21,14 @@ import Aura from '@primevue/themes/aura';
 
 const app = createApp(App);
 const pinia = createPinia();
-
+app.component('InputText', InputText)
+app.component('Button', Button)
+app.component('ProgressSpinner', ProgressSpinner)
+app.component('Card', Card)
+app.component('Dialog', Dialog)
+app.component('MultiSelect', MultiSelect)
+app.component('Checkbox', Checkbox)
+app.component('Toast', Toast)
 app.use(ToastService);
 app.use(pinia);
 app.use(PrimeVue, {
